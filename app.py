d042b7b65436c71502411420a4aabd7d754c5595
Putra, A. R. Kusuma <arifaldi13@gmail.com>
2025-11-12 10:04:19 +0700

    Update langchain

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import streamlit as st
 from langchain_openai import OpenAI
 #from langchain.sql_database import SQLDatabase
 from langchain_community.utilities import SQLDatabase
-from langchain.agents import create_sql_agent
+#from langchain.agents import create_sql_agent
+from langchain_community.agent_toolkits import create_sql_agent
 #from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from langchain_community.agent_toolkits import SQLDatabaseToolkit
 from langchain.agents.agent_types import AgentType
